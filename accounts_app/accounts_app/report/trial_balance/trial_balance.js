aa1ed44592aecc7ba807821624f0cb77bc5761cb
Shllokkk <shllokosan23@gmail.com>
2026-01-12 23:58:15 +0530

    feat(reporting): add trial balance report

---
+++ accounts_app/accounts_app/report/trial_balance/trial_balance.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2026, Shllok and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Trial Balance"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
